80992543856a305860de672ad6f51753e6d9b60d
ViktorSorokin7500 <berhaneselassie1945@gmail.com>
2024-10-03 19:54:24 +0300

    aside filters

---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import React from "react";
+import { cn } from "@/lib/utils";
+import { FilterChecboxProps, FilterCheckbox } from "./filter-checkbox";
+import { Input } from "../ui";
+
+type Item = FilterChecboxProps;
+
+interface Props {
+  title: string;
+  items: Item[];
+  defaultItems: Item[];
+  limit?: number;
+  searchInputPlaceholder?: string;
+  onChange?: (values: string[]) => void;
+  defaultValue?: string[];
+  className?: string;
+}
+
+export const CheckboxFiltersGroup: React.FC<Props> = ({
+  title,
+  items,
+  defaultItems,
+  limit = 5,
+  searchInputPlaceholder = "Search...",
+  onChange,
+  defaultValue,
+  className,
+}) => {
+  const [showAll, setShowAll] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  console.log(searchValue);
+
+  const list = showAll
+    ? items.filter((item) =>
+        item.text.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    : defaultItems.slice(0, limit);
+
+  const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <div className={cn(className)}>
+      <p className="font-bold mb-3">{title}</p>
+
+      {showAll && (
+        <div className="mb-5">
+          <Input
+            onChange={onChangeSearchInput}
+            placeholder={searchInputPlaceholder}
+            className="bg-gray-50 border-none"
+          />
+        </div>
+      )}
+
+      <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+        {list.map((item, index) => (
+          <FilterCheckbox
+            key={index}
+            text={item.text}
+            value={item.value}
+            endAdornment={item.endAdornment}
+            checked={false}
+            onCheckedChange={(ids) => console.log(ids)}
+          />
+        ))}
+      </div>
+
+      {items.length > limit && (
+        <div className={showAll ? "border-t border-t-neutral-100 mt-4" : ""}>
+          <button
+            className="text-primary mt-3"
+            onClick={() => setShowAll((prev) => !prev)}
+          >
+            {showAll ? "Show less" : "Show more"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,29 +1,47 @@
 import React from "react";
 import { cn } from "@/lib/utils";
-import { FilterCheckbox, Title } from ".";
+import { CheckboxFiltersGroup, FilterCheckbox, RangeSlider, Title } from ".";
 import { Input } from "../ui";
+import data from "@/lib/leafly_strain_data.json";
+
+import { effectTypes } from "@/lib/effects";
 
 interface Props {
-	className?: string;
+  className?: string;
 }
 
 export const Filters: React.FC<Props> = ({ className }) => {
-	return (
-		<div className={cn(className)}>
-			<Title text="Fliters" size="sm" className="mb-5 font-bold" />
+  const items = effectTypes.map((effect, i) => ({
+    text: effect,
+    value: (i + 1).toString(),
+  }));
 
-			<div className="flex flex-col gap-4">
-				<FilterCheckbox text="Top rated" value="1" />
-				<FilterCheckbox text="New" value="2" />
-			</div>
+  return (
+    <div className={cn(className)}>
+      <Title text="Fliters" size="sm" className="mb-5 font-bold" />
 
-			<div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
-				<p className="font-bold mb-3">Price:</p>
-				<div className="flex gap-3 mb-5">
-					<Input type="number" placeholder="0" min={0} max={10} />
-					<Input type="number" placeholder="10" min={1} max={10} />
-				</div>
-			</div>
-		</div>
-	);
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="New" value="1" />
+      </div>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Price:</p>
+        <div className="flex gap-3 mb-5">
+          <Input type="number" placeholder="0" min={0} max={10} />
+          <Input type="number" placeholder="10" min={1} max={10} />
+        </div>
+
+        <RangeSlider min={0} max={10} step={0.5} value={[0, 10]} />
+      </div>
+
+      <CheckboxFiltersGroup
+        title="Effects"
+        className="mt-5"
+        limit={6}
+        items={items}
+        defaultItems={items.slice(0, 6)}
+      />
+    </div>
+  );
+  <div></div>;
 };
